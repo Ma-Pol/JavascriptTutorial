8c489ab31b83191d637393744bc85491ff1851ae
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-04-15 22:06:52 +0900

    #2 변수

---
+++ 2. 변수/variable.js
@@ -0,0 +1,12 @@
+let myName = '이승현';
+console.log(myName);
+
+myName = 'YSH';
+console.log(myName + '\n');
+
+const myName2 = '이승현';
+console.log(myName2);
+
+// const 변수는 한 번 선언하면 값을 변경할 수 없다.
+// myName2 = 'YSH';
+// console.log(myName2);
