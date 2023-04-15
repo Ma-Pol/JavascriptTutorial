eb716e8774baa360b51efa2f77d7661a9e9354d8
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-04-15 23:05:48 +0900

    #3 데이터 타입 + 데이터 타입 퀴즈

---
+++ 3. 데이터 타입/dataType.js
@@ -0,0 +1,25 @@
+// 기본형 타입
+// Number
+console.log(105);
+const age = 71105;
+console.log(age);
+console.log('\n');
+
+// String
+console.log('Seung-Hyeon');
+const lastName = 'Yi';
+console.log(lastName);
+console.log('\n');
+
+// Boolean
+console.log(false);
+const isTrue = true;
+console.log(isTrue);
+console.log(false === isTrue);
+console.log('\n');
+
+// Null, Undefined
+let name1 = null;
+console.log(name1);
+let age2;
+console.log(age2);
---
+++ 3. 데이터 타입/quiz1.js
@@ -0,0 +1,9 @@
+// 내가 원하는 변수들을 선언하고, 출력해보자.
+const firstName = 'Seung-Hyeon';
+const lastName = 'Yi';
+const age = 28;
+const isMarried = false;
+
+console.log('나의 이름: ' + firstName + ' ' + lastName);
+console.log('나의 나이: ' + age);
+console.log('결혼 여부: ' + isMarried);
