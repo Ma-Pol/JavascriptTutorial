1d608d3c728679b6950f60e63b29dd4d1d1fd404
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-04-16 15:45:51 +0900

    #6 반복문

---
+++ 6. 반복문/iteration.js
@@ -0,0 +1,12 @@
+// while
+let temperature = 0;
+while (temperature < 25) {
+  console.log(`${temperature}도 정도면 적당한 온도입니다.`);
+  temperature++;
+}
+console.log('\n');
+
+// for
+for (let temperature = 0; temperature < 25; temperature++) {
+  console.log(`${temperature}도 정도면 적당한 온도입니다.`);
+}
